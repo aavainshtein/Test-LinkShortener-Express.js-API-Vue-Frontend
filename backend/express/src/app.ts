f64a88709a411ee2be807ea76b7149dc8a7daa76
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-11 16:38:39 +0200

    feat(express): set up middleware for logging and JSON parsing, set main routes

--- backend/express/src/app.ts
+++ backend/express/src/app.ts
@@ -1,9 +1,20 @@
+// backend/express/src/app.ts
 import express from 'express'
+import morgan from 'morgan'
+import { errorHandler } from './middlewares/errorHandler.middleware'
+import linkRoutes from './routes/link.routes'
 
 const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+app.use('/', linkRoutes)
+
+app.use(errorHandler)
 
 export default app
