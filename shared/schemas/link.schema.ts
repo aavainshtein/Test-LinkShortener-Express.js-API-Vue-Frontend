ed15466924cf9d384c4da8c6ef1a57a377e44bd8
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-11 16:29:07 +0200

    chore(schema): add createLinkSchema and CreateLinkDto for link validation

---
+++ shared/schemas/link.schema.ts
@@ -0,0 +1,29 @@
+// shared/schemas/link.schema.ts
+import { z } from 'zod'
+
+export const createLinkSchema = z.object({
+  originalUrl: z
+    .string()
+    .url('Invalid URL format')
+    .min(1, 'Original URL cannot be empty'),
+  expiresAt: z
+    .union([
+      z
+        .string()
+        .datetime({ message: 'Invalid ISO 8601 date format' })
+        .optional(),
+      z.null(),
+    ])
+    .optional(), // Optional, can be null or a valid ISO 8601 string
+  alias: z
+    .string()
+    .min(1, 'Alias cannot be empty')
+    .max(20, 'Alias too long')
+    .regex(
+      /^[a-zA-Z0-9_-]+$/,
+      'Alias can only contain alphanumeric characters, hyphens, and underscores',
+    )
+    .optional(),
+})
+
+export type CreateLinkDto = z.infer<typeof createLinkSchema>
