18aa345340f43a489809b15f48ef6bfa70f4a06b
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-11 19:39:35 +0200

    fix(express): update import paths to include file extensions and add IP logger middleware to redirect route

--- backend/express/src/routes/link.routes.ts
+++ backend/express/src/routes/link.routes.ts
@@ -1,14 +1,14 @@
 // backend/express/src/routes/link.routes.ts
 import { Router } from 'express'
-import { validateRequest } from '../middlewares/validateRequest.middleware'
-import { createLinkSchema } from '../../shared/schemas/link.schema' 
+import { validateRequest } from '../middlewares/validateRequest.middleware.js'
+import { ipLoggerMiddleware } from '../middlewares/ipLogger.middleware.js'
+import { createLinkSchema } from '../../shared/schemas/link.schema.js'
 import {
   createShortLinkController,
   redirectToOriginalUrlController,
-} from '../controllers/link.controller'
+} from '../controllers/link.controller.js'
 
 const router = Router()
-
 
 router.post(
   '/api/shorten',
@@ -16,7 +16,6 @@ router.post(
   createShortLinkController,
 )
 
-
-router.get('/:shortAlias', redirectToOriginalUrlController)
+router.get('/:shortAlias', ipLoggerMiddleware, redirectToOriginalUrlController)
 
 export default router
