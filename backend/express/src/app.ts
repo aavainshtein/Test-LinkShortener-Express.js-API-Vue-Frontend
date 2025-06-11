c1aea21882c0938f2e2f45727304def823557ef1
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-11 19:33:09 +0200

    fix(express): update import paths to include file extensions and set trust proxy

--- backend/express/src/app.ts
+++ backend/express/src/app.ts
@@ -1,13 +1,14 @@
 // backend/express/src/app.ts
 import express from 'express'
 import morgan from 'morgan'
-import { errorHandler } from './middlewares/errorHandler.middleware'
-import linkRoutes from './routes/link.routes'
+import { errorHandler } from './middlewares/errorHandler.middleware.js'
+import linkRoutes from './routes/link.routes.js'
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
+app.set('trust proxy', true)
 
 app.get('/', (req, res) => {
   res.send('Hello World')
