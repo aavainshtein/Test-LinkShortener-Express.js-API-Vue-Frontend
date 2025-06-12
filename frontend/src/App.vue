bdb80c009e1b1a6b7afd66563f16d00b55c13ef6
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-12 23:18:56 +0200

    feat(frontend): update header text and add contact information in App component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,9 +10,30 @@ const linksTable = useTemplateRef('linksTable')
 <template>
   <UApp :toaster="{ position: 'top-center', duration: 3000 }">
     <div class="flex flex-col items-center justify-center gap-4">
-      <h1 class="text-2xl font-bold text-(--ui-primary)">Aav link shortener</h1>
+      <h1 class="text-2xl font-bold text-(--ui-primary)">Link shortener</h1>
       <LinkInput @linkCreated="linksTable?.loadLinks" />
       <LinksTable ref="linksTable" />
+      <div class="flex flex-col items-center justify-center gap-2">
+        <span class="text-sm text-gray-500">
+          Made by Aleksandr Vainshtein
+        </span>
+        <a
+          class="text-(--ui-primary) hover:underline"
+          href="https://hh.ru/resume/cae773a8ff0ec7c1490039ed1f784741747a67"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Head-Hunter
+        </a>
+        <a
+          class="text-(--ui-primary) hover:underline"
+          href="mailto:asderq@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          asderq@gmail.com
+        </a>
+      </div>
     </div>
   </UApp>
 </template>
