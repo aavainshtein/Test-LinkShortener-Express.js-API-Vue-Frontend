cbe00cb6bec6ce4c0a16f0cccf84e2d766a61205
Aleksandr Vainshtein <asderq@gmail.com>
2025-06-12 17:32:40 +0200

    feat(frontend): integrate LinkInput and LinksTable components with dynamic link loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import LinkInput from './components/LinkInput.vue'
 import LinksTable from './components/LinksTable.vue'
+import { useTemplateRef } from 'vue'
+
+// the first argument must match the ref value in the template
+const linksTable = useTemplateRef('linksTable')
 </script>
 
 <template>
-  <UApp>
-    <div class="flex h-screen flex-col items-center justify-center gap-4">
+  <UApp :toaster="{ position: 'top-center', duration: 3000 }">
+    <div class="flex flex-col items-center justify-center gap-4">
       <h1 class="text-2xl font-bold text-(--ui-primary)">Aav link shortener</h1>
-      <LinkInput />
-      <LinksTable />
+      <LinkInput @linkCreated="linksTable?.loadLinks" />
+      <LinksTable ref="linksTable" />
     </div>
   </UApp>
 </template>
